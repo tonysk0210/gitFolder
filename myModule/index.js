1681daf867b4f0529dcd115bb7948ce1c0193974
Anthony Shangkuan <tonyshangkuan@gmail.com>
2024-01-12 15:34:49 +0800

    update date index.js

--- myModule/index.js
+++ myModule/index.js
@@ -1,7 +1,7 @@
-let app = require("./app");
+let app1 = require("./app1");
 let app2 = require("./app2");
 let app3 = require("./app3");
 
 exports.morning = app2.morning;
 exports.lunch = app3.lunch;
-exports.midNight = app.midNight;
+exports.midNight = app1.midNight;
